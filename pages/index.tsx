3924888e01efb40c094bf5f02ea38e3fa0b024c2
Sherif Wael <sherifwael80@gmail.com>
2023-04-13 12:07:18 +0200

    stlye: Remove align items center from page wrapper

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import ActivityLog from "@/components/ActivityLog";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center py-10">
+    <div className="min-h-screen flex justify-center py-10">
       <div className="w-full max-w-5xl px-4">
         <ActivityLog />
       </div>
